1118696957766b20de4bf1546e96d20bcdf71620
ocn06 <olivian@outlook.dk>
2018-05-14 11:04:18 +0200

    Slettet LoginController
    Rettet i User
    
    Funktionalitet i HomeController

--- Albertlund Hjemmepleje/Controllers/HomeController.cs
+++ Albertlund Hjemmepleje/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Albertlund_Hjemmepleje.Models.Entities;
 
 namespace Albertlund_Hjemmepleje.Controllers
 {
@@ -10,21 +11,47 @@ namespace Albertlund_Hjemmepleje.Controllers
     {
         public ActionResult Index()
         {
+            string email = Request["email"];
+            string password = Request["password"];
+
+            //datebase kald ... er brugeren oprettet?
+
+            Session["login"] = true;
             return View();
         }
 
         public ActionResult RetProfil()
         {
             return View();
+        }
 
+        public ActionResult OpretBruger()
+        {
+            string name = Request["fullname"];
+            string email = Request["email"];
+            uint phone = Convert.ToUInt16(Request["phone"]);
+            string occupation = Request["phone"];
+            string admin = Request["userType"];
+
+            User user = new User(name, email, phone, occupation, admin);
+
+            //saveToDatabase(user);
+
+            var loggedIn = Session["login"].Equals(true);
+
+            if (loggedIn == true)
+            {
+                return View();
+            }
+            else
+            {
+                return Index();
+            }
 
         }
 
-        public ActionResult OpretBruger(){
-            return View();
-        }
-
-        public ActionResult Log(){
+        public ActionResult Log()
+        {
             {
                 return View();
             }
--- Albertlund Hjemmepleje/Models/Entities/User.cs
+++ Albertlund Hjemmepleje/Models/Entities/User.cs
@@ -7,21 +7,28 @@ namespace Albertlund_Hjemmepleje.Models.Entities
 {
     public class User
     {
-            private string email;
-            private string password;
-            private string role;
-            private string name;
-            private int phone;
-            private string occupation;
+        private string email;
+        private string password;
+        private string role;
+        private string name;
+        private uint phone;
+        private string occupation;
 
-            public User(string email, string password, string role, string name, int phone, string occupation)
-            {
-                this.email = email;
-                this.password = password;
-                this.role = role;
-                this.name = name;
-                this.phone = phone;
-                this.occupation = occupation;
-            }
+        public User(string name, string email, uint phone, string occupation, string role)
+        {
+            this.Email = email;
+            this.Role = role;
+            this.Name = name;
+            this.Phone = phone;
+            this.Occupation = occupation;
         }
+
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public string Role { get => role; set => role = value; }
+        public string Name { get => name; set => name = value; }
+        public uint Phone { get => phone; set => phone = value; }
+        public string Occupation { get => occupation; set => occupation = value; }
     }
+}
+
