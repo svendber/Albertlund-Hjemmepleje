ceb1a69d683aeb2d879b82bb69cc42981320da82
svendber <svendber@hotmail.co.uk>
2018-05-17 12:14:02 +0200

    Mere lavet i person.cs

--- Albertlund Hjemmepleje/Models/Entities/Person.cs
+++ Albertlund Hjemmepleje/Models/Entities/Person.cs
@@ -8,13 +8,23 @@ namespace Albertlund_Hjemmepleje.Models.Entities
     public class Person
     {
 
-       public int iD { get; set; }
+        public int iD { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
         public string email { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
         public string name { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
         public string password { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
         public string role { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
         public string occupation { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
         public uint phone { get; set; }
+       
+
 
     }
 }
+
+
